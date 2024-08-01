ba9fb4ab1a102a16d60b72c523184ad67650f84f
Codebvoy15 <ssachink15@gmail.com>
2024-08-01 20:29:48 +0530

    Update server.js

--- Guardian Nav/live-tracking/Server/server.js
+++ Guardian Nav/live-tracking/Server/server.js
@@ -3,8 +3,8 @@ const WebSocket = require('ws');
 const app = express();
 const port = 3030;
 
-// Serve static files
-app.use(express.static('public')); // Directory for HTML, JS, and CSS
+
+app.use(express.static('public')); 
 
 app.get('/officer', (req, res) => {
     res.sendFile(__dirname + '/public/officer.html');
@@ -14,25 +14,25 @@ const server = app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
 
-// Create WebSocket server
+
 const wss = new WebSocket.Server({ noServer: true });
 
 wss.on('connection', ws => {
     console.log('Client connected');
     
     ws.on('message', message => {
-        // Convert the message from buffer to string
+       
         const messageString = message.toString();
         
-        // Parse JSON data
+        
         try {
             const data = JSON.parse(messageString);
             console.log('Received message:', data);
 
-            // Add timestamp to data
+            
             data.timestamp = Date.now();
 
-            // Broadcast the parsed message to all connected clients
+            
             wss.clients.forEach(client => {
                 if (client.readyState === WebSocket.OPEN) {
                     client.send(JSON.stringify(data));
